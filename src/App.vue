07962680786dcc2cbb063e893b025a521c2594a7
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-10-11 16:07:27 -0500

    feat: :tada: Inicio portafolio

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineAsyncComponent } from 'vue'
+const HelloWorld = defineAsyncComponent(() => import('./components/HelloWorld.vue'))
+</script>
+
+<template>
+  <div>
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
+<style scoped>
+/* ... tus estilos ... */
+</style>
