6892039e4811990cd85c413a492cf8cff6ac6e47
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-10-28 13:52:52 -0500

    feat: :wrench: arreglo del vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    outDir: 'dist', // Asegúrate de que la salida sea correcta
+  },
+});
