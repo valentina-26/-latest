f636d12c97f2b6539a09783ba36de64b54cd9700
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-10-29 06:57:17 -0500

    feat: :rocket: deployd

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'dist', // Asegúrate de que la salida sea correcta
-  },
+  base:'https://valentina-26.github.io/Mi-portafolio/',
 });
